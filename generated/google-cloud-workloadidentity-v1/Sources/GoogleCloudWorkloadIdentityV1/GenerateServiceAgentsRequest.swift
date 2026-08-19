ab2f2d88c09e86969548c28ac644ba0acac48ca8
Carlos O'Ryan <coryan@google.com>
2026-08-19 19:05:37 -0400

    Fix generated code for new library

--- generated/google-cloud-workloadidentity-v1/Sources/GoogleCloudWorkloadIdentityV1/GenerateServiceAgentsRequest.swift
+++ generated/google-cloud-workloadidentity-v1/Sources/GoogleCloudWorkloadIdentityV1/GenerateServiceAgentsRequest.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// Message for creating all ServiceAgents for a ServiceProducer in a project and
 /// location.
--- generated/google-cloud-workloadidentity-v1/Sources/GoogleCloudWorkloadIdentityV1/ServiceAgent.swift
+++ generated/google-cloud-workloadidentity-v1/Sources/GoogleCloudWorkloadIdentityV1/ServiceAgent.swift
@@ -15,7 +15,7 @@
 // limitations under the License.
 
 import Foundation
-import GoogleCloudWkt
+@_spi(GoogleCloudInternal) import GoogleCloudWkt
 
 /// Message describing ServiceAgent object
 public struct ServiceAgent: Codable, Equatable, GoogleCloudWkt._AnyPackable,
